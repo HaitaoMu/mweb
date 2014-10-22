6eb23614c5098a4ee548fafc0672f9832757f612
JetQin <qinjet@gmail.com>
2014-10-22 20:20:45 +0800

    fix bugs

---
+++ src/main/java/com/mweb/model/common/AbstractTree.java
@@ -0,0 +1,20 @@
+/***********************************************************************
+ *
+ * @file         AbstractTree.java
+ *
+ * @copyright    Copyright: 2014-2016 Usee Co. Ltd.
+ * @author       JetQin 
+ * @create-time  2014年10月22日
+ *
+ *
+ ***********************************************************************/
+package com.mweb.model.common;
+
+/**
+ * @author jet
+ *
+ */
+public class AbstractTree
+{
+
+}
